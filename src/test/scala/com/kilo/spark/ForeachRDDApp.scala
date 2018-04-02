03c603e8bc2f52d8a17639c4625de9833e3586da
kilo <642026443@qq.com>
2018-04-02 18:59:28 +0800

    使用Spark Streaming完成词频统计，并将结果写入到MySQL数据库中(未完成)

---
+++ src/test/scala/com/kilo/spark/ForeachRDDApp.scala
@@ -0,0 +1,45 @@
+package com.kilo.spark
+
+import java.sql.DriverManager
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * 使用Spark Streaming完成词频统计，并将结果写入到MySQL数据库中
+  * Created by kilo on 2018/4/2.
+  */
+object ForeachRDDApp {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("ForeachRDDApp").setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+
+    val lines = ssc.socketTextStream("sparksql", 6789)
+    val result = lines.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+    result.print()
+
+    result.foreachRDD(rdd => {
+      rdd.foreachPartition(partitionOfRecords => {
+        val connection = getConnection()
+        partitionOfRecords.foreach(record => {
+          val sql = "insert into wordcount(word,wordcount) values ('" + record._1 + "'," + record._2 + ")"
+          connection.prepareStatement(sql).execute()
+        })
+        connection.close()
+      })
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  /**
+    * 获取M有SQL的连接
+    */
+  def getConnection() = {
+    Class.forName("com.mysql.jdbc.Driver")
+    DriverManager.getConnection("jdbc:mysql://kilo:3306/sparkstreamingproject", "root", "1234")
+  }
+
+
+}
