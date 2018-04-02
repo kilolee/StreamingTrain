7b50c5aac829a03f931a4d3f051e32ff087ff288
kilo <642026443@qq.com>
2018-04-02 17:39:32 +0800

    使用Spark Streaming完成有状态统计

---
+++ src/test/scala/com/kilo/spark/StatefulWordCount.scala
@@ -0,0 +1,43 @@
+package com.kilo.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * 使用Spark Streaming完成有状态统计
+  * Created by kilo on 2018/4/2.
+  */
+object StatefulWordCount {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("StatefulWordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+
+    //如果使用了stateful的算子，必须要设置checkpoint
+    //在生产环境中，建议把checkpoint设置到HDFS的某个文件夹中
+    ssc.checkpoint(".")
+
+    val lines = ssc.socketTextStream("sparksql", 6789)
+
+    val result = lines.flatMap(_.split(" ")).map((_, 1))
+
+    val state = result.updateStateByKey[Int](updateFunction _)
+
+    state.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  /**
+    * 用当前数据去更新老的数据
+    *
+    * @param currentValues 当前的
+    * @param preValues     老的
+    * @return
+    */
+  def updateFunction(currentValues: Seq[Int], preValues: Option[Int]): Option[Int] = {
+    val current = currentValues.sum
+    val pre = preValues.getOrElse(0)
+    Some(current + pre)
+  }
+}
