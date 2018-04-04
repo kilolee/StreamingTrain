510a47060c48f93e441c43ccfa5e897cc8dbb45e
kilo <642026443@qq.com>
2018-04-04 11:19:08 +0800

    Spark Streaming整合Flume的第二种方式:pull方式

---
+++ src/main/scala/com/kilo/spark/FlumePullWordCount.scala
@@ -0,0 +1,35 @@
+package com.kilo.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.flume.FlumeUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * Spark Streaming整合Flume的第二种方式:pull方式
+  * Created by kilo on 2018/4/3.
+  */
+object FlumePullWordCount {
+  def main(args: Array[String]): Unit = {
+
+    if (args.length != 2) {
+      System.err.println("Usage: FlumePullWordCount <hostname> <port>")
+      System.exit(1)
+    }
+
+    val Array(hostname, port) = args
+
+    val sparkConf = new SparkConf().setAppName("FlumePullWordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+
+    //TODO 使用SparkStreaming整合Flume
+    val flumeStream = FlumeUtils.createPollingStream(ssc, hostname, port.toInt)
+
+    flumeStream.map(x => new String(x.event.getBody.array()).trim)
+      .flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+      .print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
